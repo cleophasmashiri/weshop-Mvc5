fbca31143c251601bc75027244bd3b4ffd4cc12e
cleophas mashiri <cleophasmashiri@gmail.com>
2015-05-28 16:50:23 +0200

    add js to check button

---
+++ Weshop/Scripts/main.js
@@ -0,0 +1,19 @@
+﻿$(window).load(function () {
+    
+    return $('#checkoutButton').click(function (e) {
+        console.log('click');
+        var $this, url;
+        e.preventDefault();
+
+        $this = $(this).closest('a');
+        url = this.data('targetUrl');
+        return $.ajax({
+            url: url,
+            type: 'put',
+            success: function (data) {
+                $('.cart-count').html(data);
+                return $this.closest('.cart-product').slideUp();
+            }
+        });
+    });
+});
